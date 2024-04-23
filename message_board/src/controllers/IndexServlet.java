b6dd06ce13f835c4f87af5760fc5af60c8bfc024
dir-FY24watanabehinata <hinata.watanabe.g848@dirbato.co.jp>
2024-04-23 09:28:16 +0900

    Index/NewServletの修正

--- message_board/src/controllers/IndexServlet.java
+++ message_board/src/controllers/IndexServlet.java
@@ -38,6 +38,12 @@ public class IndexServlet extends HttpServlet {
 		response.getWriter().append(Integer.valueOf(messages.size()).toString());
 		
 		em.close();
+		
+    request.setAttribute("messages", messages);
+
+		    var rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+		    rd.forward(request, response);
+
 
 }
 }
